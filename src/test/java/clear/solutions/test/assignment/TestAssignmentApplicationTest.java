e688acd82e835853bdb8d489246ff061fc60b9fc
Andrii Pesliak <apesliak@piraeusbank.ua>
2024-04-29 23:20:20 +0300

    Initial commit.

---
+++ src/test/java/clear/solutions/test/assignment/TestAssignmentApplicationTest.java
@@ -0,0 +1,14 @@
+package clear.solutions.test.assignment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+class TestAssignmentApplicationTest {
+
+    @Test
+    void contextLoads() {}
+
+}
